2a4317c8aafb2510e0a86b4b41f54295688f5613
Papa G <florentgerard@sfr.fr>
2022-09-13 13:47:33 +0200

    agencement code

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,15 +1,10 @@
-
 const express = require('express');
 const router = express.Router();
 const Ctrl = require('../controllers/comments');
 const JWT = require('../middlewares/authentication');
 
 router.post('/', [JWT.auth], Ctrl.createComment);
-
 router.get('/:postId', [JWT.auth], Ctrl.getComment);
-
 router.put('/update/:commentId', [JWT.auth], Ctrl.modifyComment);
-
 router.delete('/delete/:commentId', [JWT.auth], Ctrl.deleteComment);
-
 module.exports = router;
