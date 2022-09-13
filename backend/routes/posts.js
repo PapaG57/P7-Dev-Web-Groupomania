f38792f1cd90145a33db0b6e87969cb8545c6739
Papa G <florentgerard@sfr.fr>
2022-09-13 13:46:09 +0200

    correction router.get

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -5,12 +5,9 @@ const JWT = require('../middlewares/authentication');
 const upload = require('../middlewares/multer');
 
 router.post('/', [JWT.auth], upload.single('image'), Ctrl.createPost);
-
 router.get('/', [JWT.auth], Ctrl.getAllPosts);
 router.get('/:id', [JWT.auth], Ctrl.getPost);
-
 router.put('/update/:id', [JWT.auth], upload.single('image'), Ctrl.modifyPost);
-
 router.delete('/delete/:id', [JWT.auth], Ctrl.deletePost);
 
 module.exports = router;
